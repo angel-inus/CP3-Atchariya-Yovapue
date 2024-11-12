b96e9b07c88e31d96daa37ca38ccde82006c279f
angel-inus <20.ariya.y@gmail.com>
2024-11-12 10:10:53 +0700

    Exercise4 coding
    Writing code for Exercise4 from Complete Python3 Programming course by BorntoDev

---
+++ Exercise4_Atchariya_Y.py
@@ -0,0 +1,10 @@
+score1 = float(input("Enter your Foundation English score: "))
+score2 = float(input("Enter your General Business score: "))
+score3 = float(input("Enter your Introduction to Computer Systems score: "))
+score4 = float(input("Enter your Computer Programming score: "))
+
+print("--- Your Score ---")
+print("Foundation English :",score1)
+print("General Business :",score2)
+print("Introduction to Computer Systems :",score3)
+print("Computer Programming :",score4)
