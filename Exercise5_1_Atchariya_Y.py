472c692a4ada3e37190787057db4aec9802a620d
angel-inus <20.ariya.y@gmail.com>
2024-11-12 10:22:05 +0700

    Exercise5_1 coding
    Writing code for Exercise5_1 from Complete Python3 Programming course by BorntoDev

---
+++ Exercise5_1_Atchariya_Y.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+
+opr1 = num1 + num2
+opr2 = num1 - num2
+opr3 = num1 * num2
+opr4 = num1 / num2
+
+print(num1,'+',num2,'=',opr1)
+print(num1,'-',num2,'=',opr2)
+print(num1,'*',num2,'=',opr3)
+print(num1,'/',num2,'=',opr4)
